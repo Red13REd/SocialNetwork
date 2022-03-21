52600b9a1e4a6c5a747a1be4bfa1e8c5b1295b30
Red <amekysw@gmail.com>
2022-03-21 19:09:18 +0300

    TypeScript 52

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,16 +5,18 @@ import axios from "axios";
 // @ts-ignore
 import userPhoto from "../../assets/img/userAvatar.png"
 
-export const Users:React.FC<UsersType> = ({state,setUsers,toggleFollow}) => {
-    if(state.users.length === 0){
-
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response =>{
-            setUsers(response.data.items)
-        })
+export const Users: React.FC<UsersType> = ({state, setUsers, toggleFollow}) => {
+    let getUsers = () => {
+        if (state.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                setUsers(response.data.items)
+            })
+        }
     }
 
     return (
         <div>
+            <button onClick={getUsers}>get users</button>
             {
                 state.users.map(m => {
                     return (
@@ -22,7 +24,7 @@ export const Users:React.FC<UsersType> = ({state,setUsers,toggleFollow}) => {
                             <span>
                                 <div>
                                      <img className={styles.photo}
-                                          src={m.photos.small !== null? m.photos.small : userPhoto}/>
+                                          src={m.photos.small !== null ? m.photos.small : userPhoto}/>
                                 </div>
                                 <div>
                                     <button
